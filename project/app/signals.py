2b4910581f359677e29a167cf96a32985936bc4f
yogesh <yogeshmagarde1994@gmail.com>
2024-10-17 17:39:27 +0530

    updata signel

--- project/app/signals.py
+++ project/app/signals.py
@@ -7,10 +7,9 @@ from .models import Profile
 @receiver(post_save, sender=User) 
 def create_profile(sender, instance, created, **kwargs):
 	if created:
-		# breakpoint()
 		Profile.objects.create(user=instance)
 
 
-# @receiver(post_save, sender=User) 
-# def save_profile(sender, instance, **kwargs):
-# 		instance.profile.save()
+@receiver(post_save, sender=User) 
+def save_profile(sender, instance, **kwargs):
+		instance.profile.save()
